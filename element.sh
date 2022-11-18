2d528fcb6cbaae67a3b70d2d5628e3e112478aca
paqve <pq.inc@outlook.com>
2022-11-18 06:48:12 +0000

    feat: print the correct message

--- element.sh
+++ element.sh
@@ -5,13 +5,14 @@ if [[ -z $1 ]]
 then
 echo "Please provide an element as an argument."
 else 
-#echo -e"The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
   if [[ $1 =~ [0-9]+ ]] 
   then 
   GET_DATA=$($PSQL "SELECT * FROM properties full join elements using(atomic_number) WHERE atomic_number=$1 ")
   else
   GET_DATA=$($PSQL "SELECT * FROM properties full join elements using(atomic_number) WHERE  symbol='$1' or name='$1' ")
   fi
-
-
+  echo "$GET_DATA "| while read ATOMIC_NUMBER BAR TYPE BAR ATOMIC_MASS BAR MELTING_P BAR BOILING_P BAR NO BAR SYMBOL BAR NAME
+    do
+    echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_P celsius and a boiling point of $BOILING_P celsius."
+    done
 fi
